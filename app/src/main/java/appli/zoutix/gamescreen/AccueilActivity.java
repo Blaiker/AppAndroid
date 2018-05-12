03ddbeb960049141e879c2dcfef7707bffc76206
Blaker <joris_gallot@hotmail.com>
2018-05-12 23:23:21 +0200

    Premier commit Joris.

--- app/src/main/java/appli/zoutix/gamescreen/AccueilActivity.java
+++ app/src/main/java/appli/zoutix/gamescreen/AccueilActivity.java
@@ -2,6 +2,7 @@ package appli.zoutix.gamescreen;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class AccueilActivity extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class AccueilActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accueil);
+        TextView text = (TextView) findViewById(R.id.email);
     }
 }
